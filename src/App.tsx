fb24d6540e0aeb161120f26332ba5c8b45e8bc31
Mahshad Jalali <jmahshad@yahoo.com>
2023-02-04 16:22:23 -0500

    market data shows up upon search

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,133 @@ const binance = new Binance().options({
   APISECRET: 'QOxTCcskDhAlDqV1LeRl0hYFYzbpIdULlvg197l7WrcinpeKMMZhpefc09ahx0TE'
 });
 
+type tradeInfo = {
+  time: number;
+  price: string;
+  qty: string;
+};
+
+interface GenerateRowProps {
+  trades: Array<tradeInfo>;
+}
+
+interface TickerProps {
+  ticker: {[key: string]: string};
+}
+
+interface Ticker24Props {
+  ticker24: {[key: string]: string | number };
+}
+
+function GenerateRows ({trades}: GenerateRowProps): any {
+  return (trades.map((row: tradeInfo) => {
+    console.log(row.price);
+    return(
+      <tr>
+        <td>{row.time}</td>
+        <td>{row.price}</td>
+        <td>{row.qty}</td>
+      </tr>
+    );
+  }));
+};
+
 function App() {
-  const [auth, setAuth] = useState('');
-  const getMarketData = async (e: any) => {
-    e.preventDefault();
-    const symbol = e.target.previousSibling.value;
-    if (symbol.length === 6)
-      setAuth('');
-    else 
-      setAuth('Currency Pair is too short!');
+  const [symbol, setSymbol] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [trades, setTrades] = useState([]);
+  const [ticker, setTicker] = useState({});
+  const [ticker24, setTicker24] = useState({});
+  const [apiError, setApiError] = useState('');
+
+  function InputError (): any {
+    if(inputError.length) {
+      return (
+        <h4>{inputError}</h4>
+      );
+    }
   }
 
-  const SearchResultTable = () => {
-    return <h4>{auth}</h4> as any;
+  function ApiError (): any {
+    if(apiError.length) {
+      return (
+        <h4>{apiError}</h4>
+      );
+    }
   }
+
+  function Ticker ({ ticker }: TickerProps): any {
+    console.log(ticker[symbol]);
+    if (ticker && ticker[symbol]) {
+      return(
+        <div className="ticker-div">
+          <h6>The {symbol} ticker price is: {ticker[symbol]}</h6>
+        </div>
+      );
+    }
+  }
+
+  function Ticker24 ({ ticker24 }: Ticker24Props): any {
+    if (ticker24) {
+      return(
+        Object.keys(ticker24).map((key) => {
+          if (key !== 'symbol')
+            return <h6 className="ticker24-h6">{key}: {ticker24[key]}</h6>
+        })
+      );
+    }
+  }
+
+  function SearchResultTable ({trades}: GenerateRowProps): any {
+    if (trades.length) {
+      return(
+        <table>
+          <thead>
+            <tr>
+              <th>Time</th>
+              <th>Price</th>
+              <th>Quantity</th>
+            </tr>
+          </thead>
+          <tbody>
+            <GenerateRows trades={trades}/>
+          </tbody>
+        </table>
+      );
+    }
+  }
+
+  const getMarketData = async (e: any) => {
+    e.preventDefault();
+    setSymbol('');
+    const symbol = e.target.previousSibling.value;
+    if (symbol.length === 6){
+      setInputError('');
+      setSymbol(symbol);
+    }
+    else 
+      setInputError('Currency Pair is too short!');
+
+    const tickerRes = await binance.prices(symbol);
+    console.log(tickerRes);
+    if (Object.keys(tickerRes).length)
+      setTicker(tickerRes);
+
+    const ticker24Res = await binance.prevDay(symbol);
+    console.log(ticker24Res);
+    if (Object.keys(ticker24Res).length)
+      setTicker24(ticker24Res);
+      
+    const tradesRes = await binance.recentTrades(symbol);
+    if (Object.keys(tradesRes).length) {
+      const lastTen = tradesRes.slice(0,10);
+      setTrades(lastTen);
+      console.log(lastTen);
+    }
+    else 
+      setApiError('An error occured when fetching the data :(');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -30,9 +143,17 @@ function App() {
         <form>
           <input style={{fontSize: 20}} placeholder='e.g BNBBTC' required />
           <button className='search-button' onClick={getMarketData}>Search</button>
-          <SearchResultTable/>
+          <ApiError/>
+          <InputError/>
         </form>
-      </header>
+          <div className='market-data'>
+            <Ticker ticker={ticker}/>
+            <div className="ticker24-div">
+              <Ticker24 ticker24={ticker24}/>
+            </div>
+            <SearchResultTable trades={trades}/>
+          </div>
+        </header>
     </div>
   );
 }
